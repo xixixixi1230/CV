d00113e12811fba5a282ec4b4bd9a5a6f39073ca
Qinyi Luo <2945855876@qq.com>
2022-08-19 08:48:09 +0800

    1.0 前端

---
+++ Car--前端/config.py
@@ -0,0 +1,9 @@
+# 数据库的配置变量
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'car_flask'
+USERNAME = 'root'
+PASSWORD = 'qwertyjwt11'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
